86e36d62adf4abf39ba4f796ed51693426d08340
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-23 10:11:33 -0300

    Update Exe15_Valores_indertemiados.py

--- Exe15_Valores_indertemiados.py
+++ Exe15_Valores_indertemiados.py
@@ -5,7 +5,20 @@ while True:
         valores.append(x)
     else:
         break
+media = sum(valores) / len(valores)
 print(f"Foram lidos {len(valores)} valores")
 print(f"Valores na ordem que foram digitados = {valores}")
 print(f"Valores na ordem inversa = {valores[::-1]}")
-print(f"Média do valores: {round((sum(valores) / len(valores)),2)}")
+print(f"Soma dos valores = {round(sum(valores),2)}")
+print(f"Média do valores: {round(media,2)}")
+c = 0
+acima_media =[]
+abaixo_media =[]
+while c < len(valores):
+    if valores[c] > media:
+        acima_media.append(valores[c])
+    else:
+        abaixo_media.append(valores[c])
+    c += 1
+print(f"Valores acima da média = {acima_media}")
+print(f"Valores abaixo da média = {abaixo_media}")
