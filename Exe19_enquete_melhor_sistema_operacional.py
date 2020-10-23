6ff91f7a79b4d85c5f733729519b56a55f65c43c
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-23 12:44:14 -0300

    Update Exe19_enquete_melhor_sistema_operacional.py

--- Exe19_enquete_melhor_sistema_operacional.py
+++ Exe19_enquete_melhor_sistema_operacional.py
@@ -9,3 +9,34 @@ As possíveis respostas são:
 5- Mac OS
 6- Outro
 """)
+windows,unix,linux,netware,macOS,outro = 0,0,0,0,0,0
+while True:
+    opção = int(input("Digite a opção: "))
+    if opção == 1:
+        windows += 1
+    elif opção == 2:
+        unix += 1
+    elif opção == 3:
+        linux += 1
+    elif opção == 4:
+        netware += 1
+    elif opção == 5:
+        macOS += 1
+    elif opção == 6:
+        outro += 1
+    elif opção > 6:
+        print("Opção invalida, não será computado:")
+    else:
+        break
+print("Sistema operacional            Votos            %")
+print("-"*50)
+total_votos = windows + unix + linux + netware + macOS + outro
+votos_servidores= [windows,unix,linux,netware,macOS,outro]
+print(f"Windows Server:                 {windows}            {round(((windows*100) / total_votos),2)}")
+print(f"Unix:                           {unix}            {round(((unix*100) / total_votos),2)}")        
+print(f"Linux:                          {linux}              {round(((linux*100) / total_votos),2)}")
+print(f"Netware:                        {netware}               {round(((netware*100) / total_votos),2)}")
+print(f"Mac OS:                         {macOS}           {round(((macOS*100) / total_votos),2)}")
+print(f"Outro:                          {outro}            {round(((outro*100) / total_votos),2)}")
+print("-"*50)
+print(f"Total :                         {total_votos}")
